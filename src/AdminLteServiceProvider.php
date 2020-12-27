03c744b6c007946e0931cfcc44179d0dbddc2d10
mohammad javad seydi <seydi.birjand@gmail.com>
2020-12-27 10:12:21 +0330

    add asset

--- src/AdminLteServiceProvider.php
+++ src/AdminLteServiceProvider.php
@@ -12,6 +12,9 @@ class AdminLteServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/route/web.php');
+        $this->publishes([
+            __DIR__."/resource/" => public_path('/')
+        ]);
     }
     public function register()
     {
