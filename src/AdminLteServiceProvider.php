37260416b657192032211981c91c422c91c1dee4
mohammad javad seydi <seydi.birjand@gmail.com>
2021-01-04 13:49:58 +0330

    fix service provider bug

--- src/AdminLteServiceProvider.php
+++ src/AdminLteServiceProvider.php
@@ -10,12 +10,12 @@ class AdminLteServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/route/web.php');
-        $this->loadViewsFrom(__DIR__."../resource/view",'adminLTE');
+        $this->loadRoutesFrom(__DIR__.'/../route/web.php');
+        $this->loadViewsFrom(__DIR__."/../resource/view",'adminLTE');
         $this->loadMigrationsFrom(__DIR__ . "/Migrations");
         $this->publishes([
-            __DIR__."../resource/public" => public_path('/'),
-            __DIR__."../resource/view" => resource_path('/views/adminLTE')
+            __DIR__."/../resource/public" => public_path('/'),
+            __DIR__."/../resource/view" => resource_path('/views/adminLTE')
         ]);
 
     }
