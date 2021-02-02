f90e881be9e0f98e48b5ca59ee60b2e678a15e17
mohammad javad seydi <seydi.birjand@gmail.com>
2021-02-02 14:00:50 +0330

    fix view exists bug

--- src/helper/helper.php
+++ src/helper/helper.php
@@ -1,7 +1,7 @@
 <?php
 if (!function_exists('sview')){
     function sview($view = null, $data = [], $mergeData = []){
-        if (view()->exists(resource_path('adminLTE.'.$view))) {
+        if (view()->exists('adminLTE.'.$view)) {
             return view('adminLTE.'.$view, $data = [], $mergeData = []);
         }else{
             return view('adminLTE::'.$view, $data = [], $mergeData = []);
